e520e77ac17d3bf4d9bed15c3ac07f21b8e2339f
Peter Vel <p.vel@wearedoop.com>
2023-04-18 17:34:18 +0200

    Enabled platform selection

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,11 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
-import usePlatforms from '../hooks/usePlatforms';
-
-const PlatformSelector = () => {
+import usePlatforms, { Platform } from '../hooks/usePlatforms';
+interface Props {
+	selectedPlatform: Platform | null;
+	onSelectPlatform: (platform: Platform) => void;
+}
+const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
 	const { data: platforms, error } = usePlatforms();
 
 	if (error) return null;
@@ -10,11 +13,16 @@ const PlatformSelector = () => {
 	return (
 		<Menu>
 			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
-				Platforms
+				{selectedPlatform?.name ?? 'Platforms'}
 			</MenuButton>
 			<MenuList>
 				{platforms.map((platform) => (
-					<MenuItem key={platform.id}>{platform.name}</MenuItem>
+					<MenuItem
+						key={platform.id}
+						onClick={() => onSelectPlatform(platform)}
+					>
+						{platform.name}
+					</MenuItem>
 				))}
 			</MenuList>
 		</Menu>
