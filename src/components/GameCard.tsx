197ed563ddd55b33e601a30a9ddabe5d71fee0e8
Peter Vel <p.vel@wearedoop.com>
2023-04-18 15:28:31 +0200

    Loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "../services/image-url";
+import React from 'react';
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
 	game: Game;
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
 	return (
-		<Card borderRadius="10px" overflow="hidden">
+		<Card width="300px" borderRadius="10px" overflow="hidden">
 			<Image src={getCroppedImageUrl(game.background_image)}></Image>
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,10 +24,12 @@ interface FetchGamesResponse {
 const useGames = () => {
 	const [games, setGames] = useState<Game[]>([]);
 	const [error, setError] = useState("");
+	const [isLoading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const controller = new AbortController();
 
+		setLoading(true);
 		apiClient
 			.get<FetchGamesResponse>("/games", { signal: controller.signal })
 			.then((result) => {
@@ -36,12 +38,15 @@ const useGames = () => {
 			.catch((err) => {
 				if (err instanceof CanceledError) return;
 				setError(err.message);
+			})
+			.finally(() => {
+				setLoading(false);
 			});
 
 		return () => controller.abort();
 	}, []);
 
-	return { games, error };
+	return { games, error, isLoading };
 };
 
 export default useGames;
