5bfd8f92da9cf127981b3e04b5bd56ca09b08924
Peter Vel <p.vel@wearedoop.com>
2023-04-18 16:03:11 +0200

    Display genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,6 +1,5 @@
 import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
 const GameCardSkeleton = () => {
-	console.log('RENDERING SKELETON');
 	return (
 		<Card>
 			<Skeleton height="200px" />
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,18 +13,18 @@ const GameGrid = () => {
 			{error && <Text>{error}</Text>}
 			<SimpleGrid
 				columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-				spacing={10}
+				spacing={3}
 				padding="10px"
 			>
 				{isLoading &&
 					skeletons.map((skeleton) => (
-						<GameCardContainer>
-							<GameCardSkeleton key={skeleton} />
+						<GameCardContainer key={skeleton}>
+							<GameCardSkeleton />
 						</GameCardContainer>
 					))}
 				{games.map((game) => (
-					<GameCardContainer>
-						<GameCard key={game.id} game={game} />
+					<GameCardContainer key={game.id}>
+						<GameCard game={game} />
 					</GameCardContainer>
 				))}
 			</SimpleGrid>
