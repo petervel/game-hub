601263a298a995eb97cfc3033f5a747f10823a6c
Peter Vel <p.vel@wearedoop.com>
2023-04-19 16:15:59 +0200

    Created local cache for genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,17 @@
 import useData from './useData';
+import genres from '../data/genres';
 
 export interface Genre {
 	id: number;
 	name: string;
 	image_background: string;
 }
+const useGenres = () => {
+	const { data: realData, isLoading, error } = useData<Genre>('/genres');
 
-const useGenres = () => useData<Genre>('/genres');
+	const data = isLoading ? genres : realData;
+
+	return { data, isLoading: false, error };
+};
 
 export default useGenres;
