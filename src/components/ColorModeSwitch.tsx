60436dce8a77ae3ad1daf62df50e4b855b555de3
Peter Vel <p.vel@wearedoop.com>
2023-04-19 10:41:53 +0200

    Added search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,15 +1,15 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
 
 const ColorModeSwitch = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
 	return (
 		<HStack>
 			<Switch
-				isChecked={colorMode == "dark"}
+				isChecked={colorMode == 'dark'}
 				onChange={toggleColorMode}
 				colorScheme="green"
 			></Switch>
-			<Text>Dark Mode</Text>
+			<Text whiteSpace="nowrap">Dark Mode</Text>
 		</HStack>
 	);
 };
